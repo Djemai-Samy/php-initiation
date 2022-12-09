2dc45b4ee9073444e07dfba38f4e27c33b4dc714
Samdjm <sam.djm93@gmail.com>
2022-12-09 15:38:54 +0100

    delete todo

--- controllers/removetodo.php
+++ controllers/removetodo.php
@@ -2,7 +2,19 @@
 $id = $GLOBALS['_GET']['id'];
 
 //avoir la connection a la db
+include_once '../fonctions/db.php';
+$maConnection = connectToDB();
+
+var_dump($maConnection);
+
 //Preparer la requete
+$requetePreparer = mysqli_prepare($maConnection, 'DELETE FROM taches WHERE id=?');
+
 //Lier les données dans la requete
+mysqli_stmt_bind_param($requetePreparer, 'i', $id);
+
 //Executer la requete
+mysqli_stmt_execute($requetePreparer);
+
 //Rediriger user vers todo.php
+header('Location: /todo.php');
